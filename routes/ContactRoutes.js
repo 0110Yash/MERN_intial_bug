87f6c1fd62e55083a1e7a5e875557d5ac1945f51
garima-pachori <96126366+garima-pachori@users.noreply.github.com>
2023-07-02 12:21:19 +0530

    updated contact routes

--- routes/ContactRoutes.js
+++ routes/ContactRoutes.js
@@ -6,14 +6,6 @@ const {updateContacts} =require("../controllers/contactController");
 const {deleteContacts} =require("../controllers/contactController");
 const {createContacts} =require("../controllers/contactController");
 
-router.route("/").get(getContacts);
-
-router.route("/").post(createContacts);
-
-router.route("/:id").get(getContact);
-
-router.route("/:id").put(updateContacts);
-
-router.route("/:id").delete(deleteContacts);
-
+router.route("/").get(getContacts).post(createContacts);
+router.route("/:id").get(getContact).put(updateContacts).delete(deleteContacts);
 module.exports=router;
