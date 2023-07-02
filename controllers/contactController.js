4d4fab6aefda203e400e885116a24a78315b0240
garima-pachori <96126366+garima-pachori@users.noreply.github.com>
2023-07-02 12:41:40 +0530

    .

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -7,6 +7,12 @@ const getContacts=(req, res) => {
 };
 
 const createContacts=(req, res) => {
+    console.log("The request body is :", req.body);
+    const {name,email,phone}=req.body;
+    if(!name,!email,!phone){
+        res.status(400);
+        throw new Error("All fiels are necessary");
+    }
     res.status(201).json({message: "Create contacts"});
 };
 
