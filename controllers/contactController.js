fc305ab66c846e7bceb5af00f431054874d3b9a2
garima-pachori <96126366+garima-pachori@users.noreply.github.com>
2023-07-04 10:03:00 +0530

    .

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -12,7 +12,7 @@ const createContacts=asyncHandler(async (req, res) => {
     const {name,email,phone}=req.body;
     if(!name,!email,!phone){
         res.status(400);
-        throw new Error("All fiels are necessary");
+        throw new Error("All fields are necessary");
     }
     res.status(201).json({message: "Create contacts"});
 });
