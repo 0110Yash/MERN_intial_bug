b2e3eb45568af00bb29e0faae1cd4528404e12b0
garima-pachori <96126366+garima-pachori@users.noreply.github.com>
2023-07-05 19:38:16 +0530

    .

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,37 @@
 const asyncHandler=require('express-async-handler');
+const bcrypt=require("bcrypt");
+const User=require("../models/userModels");
 
 const registerUser= asyncHandler(async (req, res) => {
+    const {username, email, password}=req.body;
+    if(!username, !email, !password){
+        res.status(400);
+        throw new Error("All fields are mandatory");
+    }
+    const UserAvailable=await User.findOne({email});
+    if(UserAvailable){
+        res.status(400);
+        throw new Error("User already registered")
+    }
+
+    const hashedPassword=await bycrypt.hash(password,10);
+
+    console.log("Hashed Password",hashedPassword);
+    const user=await User.create({
+        username,
+        email,
+        password: hashedPassword,
+    });
+
+    console.log(`User created ${user}`);
+    if(user){
+        res.status(201).json({_id: user.id, email: user.email});
+    }
+    else{
+        res.status(400);
+        throw new Error("User data is not defined");
+    }
+
     res.json({message: "Register the user"});
 });
 
